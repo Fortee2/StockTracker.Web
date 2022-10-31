959f2ce7a2dad4c57bebafb1e85f1aa5f9ce58b3
Randy Costner <costner.randy@gmail.com>
2022-10-30 20:42:50 -0400

    Symbols reducer

--- StockTracker.Web/ClientApp/src/app/store.js
+++ StockTracker.Web/ClientApp/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import securitiesReducer from '../features/securities/securitiesSlice';
+import symbolsReducer from '../features/securities/SymbolsSlice';
 
 export default configureStore({
   reducer: {
     securities: securitiesReducer,
+    symbols: symbolsReducer,
   },
 })
---
+++ StockTracker.Web/ClientApp/src/features/securities/SymbolsSlice.js
@@ -0,0 +1,48 @@
+﻿import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getSymbols = createAsyncThunk("securities/symbols",
+    async () => {
+        const response = await axios.get(
+          "api/security/"
+        );
+        return response.data;
+      }
+)
+
+export const symbolSlice = createSlice(
+    {
+        name: "symbols",
+        initialState: {
+            data: [],
+            loading: "idle",
+            error: null
+        },
+        reducers: {},
+        extraReducers: (builder) => {
+            builder.addCase(getSymbols.pending, (state, action) => {
+                if (state.loading === "idle") {
+                    state.loading = "pending";
+                }
+            });
+
+            builder.addCase(getSymbols.fulfilled, (state, action) => {
+                if (state.loading === "pending") {
+                    state.loading = "loaded";
+                    state.data = action.payload;
+                }
+            });
+
+            builder.addCase(getSymbols.rejected, (state, action) => {
+                if (state.loading === "pending") {
+                    state.loading = "idle";
+                    state.error = action.error;
+                }
+            });
+        }
+    }
+);
+
+export const { toogleLoading, toggleEditor, updateSecurities } = symbolSlice.actions
+
+export default symbolSlice.reducer
