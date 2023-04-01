6a86f48c36016f23842cbb2b81eda42b05a6e2f4
Randy Costner <costner.randy@gmail.com>
2023-04-01 13:44:05 -0400

    Update AveragesController.cs
    removed hard coded number of periods

--- StockTracker.Web/Controllers/AveragesController.cs
+++ StockTracker.Web/Controllers/AveragesController.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MySqlX.XDevAPI.Common;
-using StockTracker.Core.Calculations.Response;
-using StockTracker.Core.Interfaces.Calculations;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;   
 using StockTracker.Business.DTO;
-using StockTracker.Core.Entities;
 using StockTracker.Business.Enumerations;
-using  StockTracker.Infrastructure.Repository.Interfaces;
 using StockTracker.Business.Services.Interfaces;
-using StockTracker.Business.Services;
+using StockTracker.Infrastructure.Repository.Interfaces;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -40,12 +29,11 @@ namespace StockTracker.Web.Controllers
         [HttpGet()]
         public List<MADto> GetMovingAverages(int tickerId, AverageTypes averageTypeEnum )
         {
-            //TODO:  Fix hard code number of periods.
-            return _averageService.RetrieveDataForAverageCalculations(tickerId, averageTypeEnum,9, null);
+            return _averageService.RetrieveDataForAverageCalculations(tickerId, averageTypeEnum, null);
         }
 
         [HttpGet("[action]")]
-        public  IActionResult UpdateMovingAverages()
+        public IActionResult UpdateMovingAverages()
         {
             var tickers = _securitiesService.RetriveveAll();
             _averageService.CalculateAllAverages(tickers);
